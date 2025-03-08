16973c1c6d31b87eb8bd42aaf85803b0f42b2d75
Enes Kutay SEZEN <eneskutaysezen@gmail.com>
2025-03-08 19:11:45 +0300

    Frontend basics

--- backend/src/types.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const Task = z.object({
-  name: z.string(),
-  slug: z.string(),
-  description: z.string().optional(),
-  completed: z.boolean().default(false),
-  due_date: z.string().datetime(),
-});
